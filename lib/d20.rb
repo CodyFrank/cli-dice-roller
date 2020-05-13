1daeb36e9b50b12cec415a1032518a3de6b73a84
Cody Frank <cody@Codys-MacBook-Air.local>
2020-05-12 20:41:41 -0400

    adds crit success and fail on d20

--- lib/d20.rb
+++ lib/d20.rb
@@ -1,6 +1,12 @@
 class D20 < Dice
     def roll
         @value = rand(19) + 1
-        puts "you rolled a #{@value} on the d20"
+        if @value == 20
+            puts "You rolled a natrual 20 Congrats!"
+        elsif @value == 1
+            puts "You rolled a Critical failure (1) better luck next time"
+        else
+            puts "you rolled a #{@value} on the d20"
+        end
     end
 end 
